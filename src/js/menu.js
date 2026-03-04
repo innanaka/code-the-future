1692249a1e8899991b78e903625758f0d50aa6e5
NadinKorolenko <nadinkorolenko@gmail.com>
2026-03-04 20:22:09 +0100

    fixed js

--- src/js/menu.js
+++ src/js/menu.js
@@ -13,4 +13,13 @@
       refs.modal.classList.toggle("is-open");
       document.body.classList.toggle("no-scroll");
     }
+
+     // Додаємо закриття меню при кліку на будь-який пункт
+  const menuLinks = document.querySelectorAll(".mobile-menu-link");
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      refs.modal.classList.remove("is-open");
+      document.body.classList.remove("no-scroll");
+    });
+  });
   })();
